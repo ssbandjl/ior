9774d7d49f68e7c881c580e4e12f08fef4a9e890
xb <ssbandjl@gmail.com>
2025-07-19 16:46:02 +0800

    u

--- run_2node_cx_mpi.sh
+++ run_2node_cx_mpi.sh
@@ -17,10 +17,19 @@ COMMENT
 #     -env INCLUDE=/root/project/hpc/mpi/daos/mpich-3.4.3/install/include \
 #     /root/project/hpc/ior/install/bin/ior -a POSIX -b 2G -t 128M -v -W -w -r -R -i 2 -F -vvv -o /tmp/sxb/testfile > "$log_file" 2>&1
 
-# mdtest
+# mdtest file create/stat/remove/read
+# log_file=log/cx/hpc_mdtest_$(date +'%Y_%m_%d_%H_%M_%S')_log
+# echo $log_file
+# /root/project/hpc/mpi/daos/mpich-3.4.3/install/bin/mpirun -np 2 -f /root/project/hpc/ior/mpi_hostfile \
+#     -env LD_LIBRARY_PATH=/root/project/hpc/mpi/daos/mpich-3.4.3/install/lib \
+#     -env INCLUDE=/root/project/hpc/mpi/daos/mpich-3.4.3/install/include \
+#     /root/project/hpc/ior/install/bin/mdtest -a POSIX -z 3 -F -C -T -E -r -i 2 -y -n 3334 -e 4096 -w 4096 -d /tmp/sxb/ -vvv > "$log_file" 2>&1
+
+
+# mdtest file create/stat/remove/read, sync, 8k, 10000 files
 log_file=log/cx/hpc_mdtest_$(date +'%Y_%m_%d_%H_%M_%S')_log
 echo $log_file
 /root/project/hpc/mpi/daos/mpich-3.4.3/install/bin/mpirun -np 2 -f /root/project/hpc/ior/mpi_hostfile \
     -env LD_LIBRARY_PATH=/root/project/hpc/mpi/daos/mpich-3.4.3/install/lib \
     -env INCLUDE=/root/project/hpc/mpi/daos/mpich-3.4.3/install/include \
-    /root/project/hpc/ior/install/bin/mdtest -a POSIX -z 3 -F -C -T -E -r -i 2 -n 3334 -e 4096 -d /tmp/sxb/ -w 4096 -vvv > "$log_file" 2>&1
+    /root/project/hpc/ior/install/bin/mdtest -a POSIX -z 3 -F -C -T -E -r -i 5 -y -n 10000 -e 8192 -w 8192 -d /tmp/sxb/ -vvv > "$log_file" 2>&1
--- scripts/run_check_mdtest_result.sh
+++ scripts/run_check_mdtest_result.sh
@@ -1,4 +1,4 @@
-run_cmd "ls -alh /tmp/sxb/test-dir.0-0/mdtest_tree.0|wc -l"
-run_cmd "du -sh /tmp/sxb/test-dir.0-0/"
+run_cmd "ls -alh /tmp/sxb/"
+run_cmd "du -sh /tmp/sxb/"
 run_cmd "rm -rf /tmp/sxb/*"
 
